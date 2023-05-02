4dc8f1d4dca61ef41a6d38b400895deefae368cc
Richard Hounslow <rhounslow@gmail.com>
2023-05-02 18:27:38 +1000

    get auth0 going

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div><h1>This is the homepage</h1></div>;
+  return <div><h1>This is the homepage</h1>
+    <div>
+      <Link href="/api/auth/login">
+        Login
+      </Link>
+    </div>
+  </div>;
 }
