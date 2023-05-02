35f544612b6c5b8badd74c0d5047b78762a23098
Richard Hounslow <rhounslow@gmail.com>
2023-05-03 09:57:14 +1000

    create a layout component for csstailwind

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,14 @@ import { UserProvider } from '@auth0/nextjs-auth0/client'
 
 //wraps entire app
 function MyApp({ Component, pageProps }) {
-  return <UserProvider><Component {...pageProps} /></UserProvider>
+  // get the layout for the page or use the default layout
+  const getLayout = Component.getLayout || ((page) => page)
+
+  return (
+  <UserProvider>
+    {/* pass the page component w/ its props */}
+    {getLayout(<Component {...pageProps} />, pageProps)}
+  </UserProvider>)
 }
 
 export default MyApp
--- pages/post/new.js
+++ pages/post/new.js
@@ -1,10 +1,16 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0"
+import { AppLayout } from "../../components/AppLayout"
 
-export default function New(props) {
+export default function NewPost(props) {
     const { test } = props
     console.log('test: ', test)
 
     return (<div><h1>This is a new post page</h1></div>)
+}
+
+//common next js pattern to wrap a page in a layout - refer to pages\_app.js
+NewPost.getLayout = function getLayout(page, pageProps) {
+    return <AppLayout {...pageProps}>{page}</AppLayout>
 }
 
 export const getServerSideProps = withPageAuthRequired(() => {
