83e0a1364d478fe66434a95512bdccdf1481e96c
Richard Hounslow <rhounslow@gmail.com>
2023-05-03 09:09:15 +1000

    put some user details on the home page (inc. image)

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,34 @@
 import Link from "next/link";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
 
 export default function Home() {
   const { user } = useUser();
 
   console.log("user: ", user)
 
-  return <div><h1>This is the homepage</h1>
+  return (
     <div>
-      {!!user ? (
-        <div>
-          <h1>all the  user info</h1>
-          <Link href="/api/auth/logout">
-            Logout
-          </Link>
-        </div>
-      ) : (
-        <Link href="/api/auth/login">
-          Login
-        </Link>)
-      }
+      <h1>This is the homepage</h1>
+      <div>
+        {!!user ? (
+          <>
+            <div>
+              <Image src={user.picture} alt={user.name} height={50} width={50} />
+            </div>
+            <div>
+              {user.email}
+            </div>
+            <Link href="/api/auth/logout">
+              Logout
+            </Link>
+          </>
+        ) : (
+          <Link href="/api/auth/login">
+            Login
+          </Link>)
+        }
+      </div>
     </div>
-  </div>;
+  )
 }
