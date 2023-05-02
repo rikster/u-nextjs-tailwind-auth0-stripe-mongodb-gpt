fa7207db2058356562ba096f6b46a191f801850f
Richard Hounslow <rhounslow@gmail.com>
2023-05-02 17:07:27 +1000

    add static ssr routes

---
+++ pages/post/new.js
@@ -0,0 +1,3 @@
+export default function New() {
+    return <div><h1>This is a new post page</h1></div>;
+}
---
+++ pages/token-topup.js
@@ -0,0 +1,3 @@
+export default function TokenTopup() {
+    return <div><h1>This is the token topup page</h1></div>;
+}
