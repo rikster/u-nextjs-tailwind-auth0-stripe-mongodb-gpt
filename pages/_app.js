fc8b29b60fb9a8ed604d9eda4622f0daf78ae679
Richard Hounslow <rhounslow@gmail.com>
2023-05-03 08:36:42 +1000

    auth0 userprovider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
+import { UserProvider } from '@auth0/nextjs-auth0/client'
 
+//wraps entire app
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <UserProvider><Component {...pageProps} /></UserProvider>
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Home() {
+  const { user } = useUser();
+
+  console.log("user: ", user)
+
   return <div><h1>This is the homepage</h1>
     <div>
       <Link href="/api/auth/login">
