8e4b4a20bdfc8b4194c63aee891a6206d6de7ec9
Richard Hounslow <rhounslow@gmail.com>
2023-05-03 13:08:18 +1000

    initial app layout skeleton

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,7 +1,18 @@
 export const AppLayout = ({ children }) => {
-    return (
-        <div>this is the app layout
-            <div>{children}</div>
+  return (
+    <div className="grid h-screen max-h-screen grid-cols-[300px_1fr]">
+      <div className="flex flex-col overflow-hidden text-white">
+        <div className="bg-slate-800">
+          <div>logo</div>
+          <div>cta button</div>
+          <div>tokens</div>
         </div>
-    )
-}
+        <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
+          list of posts
+        </div>
+        <div className="bg-cyan-800">user info - logout button</div>
+      </div>
+      <div className="">{children}</div>
+    </div>
+  );
+};
