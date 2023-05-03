2d6bbb3a96a0a95abb266135cd8f8c10124b94ec
Richard Hounslow <rhounslow@gmail.com>
2023-05-03 13:44:19 +1000

    style user info in footer

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -16,19 +16,24 @@ export const AppLayout = ({ children }) => {
         <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
           list of posts
         </div>
-        <div className="bg-cyan-800">
+        <div className="flex h-20 items-center gap-2 border-t border-t-black/50 bg-cyan-800 px-2">
           {!!user ? (
             <>
-              <div>
+              <div className="min-w-[50px]">
                 <Image
                   src={user.picture}
                   alt={user.name}
                   height={50}
                   width={50}
+                  className="rounded-full"
                 />
               </div>
-              <div>{user.email}</div>
-              <Link href="/api/auth/logout">Logout</Link>
+              <div className="flex-1">
+                <div className="font-bold">{user.email}</div>
+                <Link className="text-sm" href="/api/auth/logout">
+                  Logout
+                </Link>
+              </div>
             </>
           ) : (
             <Link href="/api/auth/login">Login</Link>
