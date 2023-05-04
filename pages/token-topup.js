29852f30ab8376803c937e38d04aab0baded3eff
Richard Hounslow <rhounslow@gmail.com>
2023-05-05 09:33:21 +1000

    add layout

--- pages/token-topup.js
+++ pages/token-topup.js
@@ -1,12 +1,21 @@
-import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { AppLayout } from '../components/AppLayout';
 
 export default function TokenTopup() {
-    return <div><h1>This is the token topup page</h1></div>;
+  return (
+    <div>
+      <h1>This is the token topup page</h1>
+    </div>
+  );
 }
 
+//common next js pattern to wrap a page in a layout - refer to pages\_app.js
+TokenTopup.getLayout = function getLayout(page, pageProps) {
+  return <AppLayout {...pageProps}>{page}</AppLayout>;
+};
+
 export const getServerSideProps = withPageAuthRequired(() => {
-    return {
-        props: {
-        }
-    }
-})
+  return {
+    props: {},
+  };
+});
