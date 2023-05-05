e9293649334d788012efd97faa75979ab7ab2170
Richard Hounslow <rhounslow@gmail.com>
2023-05-05 10:03:36 +1000

    style posts button and tokens avail

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useUser } from '@auth0/nextjs-auth0/client';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoins } from '@fortawesome/free-solid-svg-icons';
 
 export const AppLayout = ({ children }) => {
   const { user } = useUser();
@@ -8,7 +10,7 @@ export const AppLayout = ({ children }) => {
   return (
     <div className="grid h-screen max-h-screen grid-cols-[300px_1fr]">
       <div className="flex flex-col overflow-hidden text-white">
-        <div className="bg-slate-800">
+        <div className="bg-slate-800 px-2">
           <div>logo</div>
           <Link
             href="/post/new"
@@ -16,7 +18,10 @@ export const AppLayout = ({ children }) => {
           >
             New Post
           </Link>
-          <Link href="/token-topup">tokens</Link>
+          <Link href="/token-topup" className="mt-2 block text-center ">
+            <FontAwesomeIcon icon={faCoins} className="text-yellow-500 " />
+            <span className="pl-1">0 tokens available</span>
+          </Link>
         </div>
         <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
           list of posts
