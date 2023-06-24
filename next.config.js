7a1659c1067f8e6b1f0db0a3d969a1cba247f70f
Richard Hounslow <rhounslow@gmail.com>
2023-06-24 13:29:31 +1000

    fix logo url config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['s.gravatar.com'],
+    domains: ['s.gravatar.com', 'lh3.googleusercontent.com'],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
