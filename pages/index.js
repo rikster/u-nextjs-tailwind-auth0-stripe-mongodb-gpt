67ced78c962f98a04707fa3dcd8793bfbffa4da8
Richard Hounslow <rhounslow@gmail.com>
2023-06-28 15:44:10 +1000

    add home page webp image

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,12 @@
 import Link from 'next/link';
 import { useUser } from '@auth0/nextjs-auth0/client';
 import Image from 'next/image';
+import HeroImage from '../public/hero.webp';
 
 export default function Home() {
-  const { user } = useUser();
-
-  console.log('user: ', user);
-
   return (
-    <div>
-      <h1>This is the homepage</h1>
-      <div>
-        {!!user ? (
-          <>
-            <div>
-              <Image
-                src={user.picture}
-                alt={user.name}
-                height={50}
-                width={50}
-              />
-            </div>
-            <div>{user.email}</div>
-            <Link href="/api/auth/logout">Logout</Link>
-          </>
-        ) : (
-          <Link href="/api/auth/login">Login</Link>
-        )}
-      </div>
+    <div className="relative flex h-screen w-screen items-center justify-center overflow-hidden">
+      <Image src={HeroImage} alt="Hero" fill className="absolute" />
     </div>
   );
 }
